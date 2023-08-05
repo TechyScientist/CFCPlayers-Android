e4ff4ab1a6dd716ba1aa746a4895e549c8fc9e5c
Johnny Console <johnny.console@hotmail.com>
2023-08-05 13:23:29 -0400

    Initial Android Studio Commit

---
+++ app/src/main/java/net/johnnyconsole/cfcplayers/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.johnnyconsole.cfcplayers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
